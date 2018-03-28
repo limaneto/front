6b2bdd7f7a15f3b95e9bddece19252891b9d2569
Lima Neto <limanetocc@gmail.com>
2018-03-28 19:18:58 -0300

    First commit.

---
+++ models/user.js
@@ -0,0 +1,19 @@
+(function () {
+  class User {
+    constructor(name, cpf, phone, email) {
+      this.name = name;
+      this.cpf = cpf;
+      this.phone = phone;
+      this.email = email;
+    }
+  }
+
+  User.prototype.example = function () {
+  };
+
+  if (self && self.self) {
+    window.easyInvest = window.easyInvest || {};
+    window.easyInvest.models = window.easyInvest.models || [];
+    window.easyInvest.models.push({ User: User });
+  }
+})();
