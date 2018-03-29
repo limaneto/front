8afe29f9604969fcbc674ca01fd158cb9e6dd8c9
Lima Neto <limanetocc@gmail.com>
2018-03-28 22:23:20 -0300

    Add getUsers function.

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,27 @@
 (function () {
+
+  /*
+    Returns a array of objects
+  * */
+  let getUsers = () => {
+    if (!localStorage.getItem('users')) {
+      alert('There is no user registered.')
+    } else {
+      return JSON.parse(localStorage.getItem('users')) || [];
+    }
+  }
+
   let saveUser = user => {
-    let users = JSON.parse(localStorage.getItem('users')) || [];
-    users.push(user)
-    localStorage.setItem('users', JSON.stringify(users));
+    if (isUserRegistered(user)) {
+      alert('Email already in use.')
+    } else {
+      let users = JSON.parse(localStorage.getItem('users')) || [];
+      users.push(user)
+      localStorage.setItem('users', JSON.stringify(users));
+    }
   }
 
   if (self && self.self) {
-    window.easyInvest.utils = { saveUser}
+    window.easyInvest.utils = { saveUser, getUsers }
   }
 })();
