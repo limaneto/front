4c84b869c3d44ef87fbd4ab0617251632b039258
Lima Neto <limanetocc@gmail.com>
2018-04-01 13:13:37 -0300

    Removing unused class.

--- js/templates/footerEditProfile.js
+++ js/templates/footerEditProfile.js
@@ -2,5 +2,5 @@ window.templates = window.templates || {};
 window.templates.footerEditProfileTemplate = function () {
   return `<div><button class="button" id="delete">Deletar</button>
           <button class="button" id="edit">Atualizar</button>
-           <a href="#usersList base">Users List</a></div>`
+           <a href="#usersList">Users List</a></div>`
 }
